56c691c1879244e199c393b496545b1a1838bef0
SE13codeur <se13codeur@gmail.com>
2022-06-23 12:30:14 +0200

    implemented routing for movies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -16,7 +17,8 @@ import { MoviesListComponent } from './components/movies-list/movies-list.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/movies-list/movies-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from 'src/app/interfaces/movie';
+import { MovieService } from 'src/app/services/movie.service';
+
+@Component({
+  selector: 'app-movies-list',
+  templateUrl: './movies-list.component.html',
+  styleUrls: ['./movies-list.component.css']
+})
+export class MoviesListComponent implements OnInit {
+  movies: Movie[] = [];
+
+  constructor(private mv: MovieService) { }
+
+  ngOnInit(): void {
+    this.initMovie();
+  }
+
+  initMovie() {
+    this.mv.getAllMovies().subscribe(res => {
+      this.movies = res;
+    })
+  }
+
+}
