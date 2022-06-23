97213d012d2b2565d6e8a9c072978631c16298b3
SE13codeur <se13codeur@gmail.com>
2022-06-23 17:06:41 +0200

    added routing for auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './components/auth/auth.component';
 import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
-  { path: 'movies', component: MoviesListComponent },
+  { path: 'movies', component: MoviesListComponent, canActivate: [AuthGuard] },
+  { path: 'auth', component: AuthComponent },
 ];
 
 @NgModule({
