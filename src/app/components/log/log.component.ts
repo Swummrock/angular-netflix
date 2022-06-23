8107791e2b7aadd0a805e22a071823ace267cd0d
SE13codeur <se13codeur@gmail.com>
2022-06-24 00:43:03 +0200

    added log component and token service

--- src/app/components/log/log.component.ts
+++ src/app/components/log/log.component.ts
@@ -1,12 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/interfaces/user';
+import { TokenStorageService } from 'src/app/services/auth/token.service';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
-  selector: 'app-log',
+  selector: 'app-login',
   templateUrl: './log.component.html',
   styleUrls: ['./log.component.css']
 })
-export class LogComponent {
+export class LogComponent implements OnInit {
+  user: User = {};
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
 
-  constructor() { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
+  ngOnInit(): void {
+    if (this.tokenStorage.getToken()) {
+      this.isLoggedIn = true;
+    }
+  }
+
+  onSubmit(): void {
+    this.authService.login(this.user).subscribe({
+      next: data => {
+        // this.tokenStorage.saveUser(data);
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.reloadPage();
+      },
+      error: err => {
+        this.errorMessage = err.error.message;
+        this.isLoginFailed = true;
+      }
+    });
+  }
+
+  reloadPage(): void {
+    window.location.reload();
+  }
 }
