fcdfa9ab928386cae6942e741cae898ea757feef
SE13codeur <se13codeur@gmail.com>
2022-06-23 11:52:44 +0200

    modified routing movies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'movies', component: MoviesListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
