878dad0793a39aa82593f0cead42745adf5f54dc
SE13codeur <se13codeur@gmail.com>
2022-06-23 12:29:33 +0200

    added service movies

---
+++ src/app/services/movie.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Movie } from '../interfaces/movie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  private url: string = "http://localhost:8000/api/movies";
+
+  constructor(private http: HttpClient) { }
+
+  getAllMovies() {
+    return this.http.get<Movie[]>(this.url, {})
+  }
+
+
+}
